b1815ad67395dbb9da8b0c13d2c71c9aa50228c4
Sotaro Yagi <senngikumaru2000@yahoo.co.jp>
2023-07-10 19:24:20 +0900

    rfm

--- resources/js/Pages/Analysis.vue
+++ resources/js/Pages/Analysis.vue
@@ -1,55 +1,132 @@
-<?php
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import { reactive, onMounted } from 'vue';
+import { getToday } from '@/common';
+import Chart from '@/Components/Chart.vue';
+import ResultTable from '@/Components/ResultTable.vue';
 
-namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use App\Models\Order;
-use Illuminate\Support\Facades\DB;
-use App\Services\AnalysisService;
-use App\Services\DecileService;
-use App\Services\RFMService;
+onMounted(() => {
+    form.startDate = getToday()
+    form.endDate = getToday()
+})
 
-class AnalysisController extends Controller
-{
-    public function index(Request $request)
-    {
-        $subQuery = Order::betweenDate($request->startDate, $request->endDate);
+const form = reactive({
+    startDate: null,
+    endDate: null,
+    type: 'perDay',
+    rfmPrms: [
+        14, 28, 60, 90, 7, 5, 3, 2, 300000, 200000, 100000, 30000
+    ],
+})
 
-        if($request->type === 'perDay'){
-           list($data, $labels, $totals) = AnalysisService::perDay($subQuery);
-        }
+const data = reactive({})
 
-        if($request->type === 'perMonth'){
-            list($data, $labels, $totals) = AnalysisService::perMonth($subQuery);
-         }
-
-         if($request->type === 'perYear'){
-            list($data, $labels, $totals) = AnalysisService::perYear($subQuery);
-         }
-
-         if($request->type === 'decile'){
-            list($data, $labels, $totals) = DecileService::decile($subQuery);
-         }
-
-         if($request->type === 'rfm'){
-            list($data, $totals, $eachCount) = RFMService::rfm($subQuery, $request->rfmPrms);
-
-            return response()->json([
-                'data' => $data,
-                'type' => $request->type,
-                'eachCount' => $eachCount,
-                'totals' => $totals,
-            ], Response::HTTP_OK);
-
-        }
-        return response()->json([
-            'data' => $data,
-            'type' => $request->type,
-            'labels' => $labels,
-            'totals' => $totals,
-        ], Response::HTTP_OK);
-
+const getData = async () => {
+    try {
+        await axios.get('/api/analysis/', {
+            params: {
+                startDate: form.startDate,
+                endDate: form.endDate,
+                type: form.type,
+                rfmPrms: form.rfmPrms
+            }
+        })
+            .then(res => {
+                data.data = res.data.data
+                if(res.data.labels) { data.labels = res.data.labels }
+                if(res.data.eachCount) { data.eachCount = res.data.eachCount }
+                data.totals = res.data.totals
+                data.type = res.data.type
+                console.log(res.data)
+            })
+    } catch (e) {
+        console.log(e.message)
     }
 }
+
+
+</script>
+
+<template>
+    <Head title="データ分析" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">データ分析</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <form @submit.prevent="getData">
+                            分析方法<br>
+                            <input type="radio" v-model="form.type" value="perDay" class="mr-1" checked><span
+                                class="mr-4">日別</span>
+                            <input type="radio" v-model="form.type" value="perMonth" class="mr-1"><span
+                                class="mr-4">月別</span>
+                            <input type="radio" v-model="form.type" value="perYear" class="mr-1"><span
+                                class="mr-4">年別</span>
+                            <input type="radio" v-model="form.type" value="decile" class="mr-1"><span
+                                class="mr-4">デシル分析</span>
+                            <input type="radio" v-model="form.type" value="rfm" class="mr-1"><span
+                                class="mr-4">RFM分析</span><br>
+                            From: <input type="date" name="startDate" v-model="form.startDate">
+                            To: <input type="date" name="endDate" v-model="form.endDate"><br>
+
+                            <div v-if="form.type === 'rfm'" class="my-8">
+                                <table class="mx-auto">
+                                    <thead>
+                                        <tr>
+                                            <th>ランク</th>
+                                            <th>R (○日以内)</th>
+                                            <th>F (○回以上)</th>
+                                            <th>M (○円以上)</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>5</td>
+                                            <td><input type="number" v-model="form.rfmPrms[0]"></td>
+                                            <td><input type="number" v-model="form.rfmPrms[4]"></td>
+                                            <td><input type="number" v-model="form.rfmPrms[8]"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>4</td>
+                                            <td><input type="number" v-model="form.rfmPrms[1]"></td>
+                                            <td><input type="number" v-model="form.rfmPrms[5]"></td>
+                                            <td><input type="number" v-model="form.rfmPrms[9]"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>3</td>
+                                            <td><input type="number" v-model="form.rfmPrms[2]"></td>
+                                            <td><input type="number" v-model="form.rfmPrms[6]"></td>
+                                            <td><input type="number" v-model="form.rfmPrms[10]"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>2</td>
+                                            <td><input type="number" v-model="form.rfmPrms[3]"></td>
+                                            <td><input type="number" v-model="form.rfmPrms[7]"></td>
+                                            <td><input type="number" v-model="form.rfmPrms[11]"></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                            <button
+                                class="flex mt-4 mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">分析する</button>
+                        </form>
+
+                        <div v-show="data.data">
+                            <div v-if="data.type != 'rfm'">
+                                <Chart :data="data" />
+                            </div>
+                            <ResultTable :data="data" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+</AuthenticatedLayout></template>
